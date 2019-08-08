e997feff53400fe6d6b21a301340ea40a385d573
Slenderchat <steeplike@gmail.com>
2019-08-08 03:13:16 +0300

    Added newlines and EOF, and build script overhaul

--- build.sh
+++ build.sh
@@ -3,10 +3,29 @@ ER=1
 chk () {
 	if [  $? -eq 0  ]
 	then
-		echo "$1 Succesful";
+		echo "$1 succesful";
 		let ER++;
 	else
 		echo "$1 failed";
+		exit $ER;
+	fi
+}
+chkBld () {
+	if [  $? -eq 0  ]
+	then
+		echo "Build succesful";
+		let ER++;
+	else
+		echo "Build failed";
+		make -j 1
+		if [  $? -eq 0  ]
+		then
+			echo "Second build attempt succesful";
+			let ER++;
+		else
+			echo "Second build attempt failed";
+			exit $ER;
+		fi
 		exit $ER;
 	fi
 }
@@ -15,17 +34,22 @@ build () {
 	chk "Clean .config"
 	rm -rf files/*
 	chk "Clean files"
-	cp -f "../openwrt-config/$1/.config" ".config"
+	cp -f ../openwrt-config/$1/.config .config
 	chk "Copy .config"
-	cp -rf "../openwrt-config/$1/etc" "files"
+	cp -rf ../openwrt-config/$1/etc files
 	chk "Copy files"
 	make clean
 	chk "Clean"
 	make download
 	chk "Download"
 	make -j 1
-	chk "Build"
-	cp -f "bin/targets/*/*/openwrt-*-squashfs-*.bin" "../openwrt-firmware"
+	chkBld
+	for file in bin/targets/*/*/openwrt-*-squashfs-*.bin
+	do
+		file2=`basename $file`
+		file2=${file2##*-*-*-*-*-*-*-}
+		mv $file "${file%%openwrt-*-squashfs-*.bin}${1}-${file2}"
+	done
 	chk "Copy results"
 }
 build "sverdlova-1"
@@ -34,3 +58,4 @@ build "danilevskii-1"
 build "danilevskii-2"
 build "danilevskii-3"
 build "danilevskii-4"
+
